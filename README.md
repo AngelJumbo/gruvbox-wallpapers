5cab652c6ce5b88b4bc885480b7813ab94845b75
Angel Jumbo <anarjumb@gmail.com>
2022-07-14 14:04:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gruvbox Wallpapers
+A place where to find gruvbox theme wallpapers.
+## Contrib
+Just add the wallpapers that you have in their respective folder and run the genPage.sh script
---
+++ genPage.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+touch ./index.html
+
+echo "<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Gruvbox wallpapers</title>
+</head>
+
+<body>
+  <h1>Wallpaperss for gruvbox</h1>
+  <hr>
+  <p> Contrib here: .... </p>" > ./index.html
+
+echo "<h2>IRL</h2>" >>./index.html
+for i in ./wallpapers/irl/*
+do
+  echo "  <a target=\"_blank\" href=\"$i\">
+    <img src=\"$i\" alt="$i" width=\"200\">
+  </a>" >> ./index.html
+done
+echo "<h2>Minimalistic</h2>" >>./index.html
+for i in ./wallpapers/minimalistic/*
+do
+  echo "  <a target=\"_blank\" href=\"$i\">
+    <img src=\"$i\" alt="$i" width=\"200\">
+  </a>" >> ./index.html
+done
+echo "<h2>Mix</h2>" >>./index.html
+for i in ./wallpapers/mix/*
+do
+  echo "  <a target=\"_blank\" href=\"$i\">
+    <img src=\"$i\" alt="$i" width=\"200\">
+  </a>" >> ./index.html
+done
+echo "<h2>Pixelart</h2>" >>./index.html
+for i in ./wallpapers/pixelart/*
+do
+  echo "  <a target=\"_blank\" href=\"$i\">
+    <img src=\"$i\" alt="$i" width=\"200\">
+  </a>" >> ./index.html
+done
+echo "<h2>Anime</h2>" >>./index.html
+for i in ./wallpapers/anime/*
+do
+  echo "  <a target=\"_blank\" href=\"$i\">
+    <img src=\"$i\" alt="$i" width=\"200\">
+  </a>" >> ./index.html
+done
+
+
+
+echo "</body>
+
+</html>" >> ./index.html
