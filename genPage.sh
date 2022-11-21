a77c657128fee923de64497f7bcf4852b9f9280a
AngelJumbo <anarjumb@gmail.com>
2022-11-20 23:20:27 -0500

    Lazy loading

--- genPage.sh
+++ genPage.sh
@@ -3,11 +3,11 @@
 touch ./index.html
 
 echo "<!DOCTYPE html>
-<html lang="en">
+<html lang=\"en\">
 
 <head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset=\"utf-8\">
+  <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
   <title>Gruvbox wallpapers</title>
 </head>
 
@@ -20,42 +20,42 @@ echo "<h2>IRL</h2>" >>./index.html
 for i in ./wallpapers/irl/*
 do
   echo "  <a target=\"_blank\" href=\"$i\">
-    <img src=\"$i\" alt="$i" width=\"200\">
+    <img loading=\"lazy\" src=\"$i\" alt="$i" width=\"200\">
   </a>" >> ./index.html
 done
 echo "<h2>Minimalistic</h2>" >>./index.html
 for i in ./wallpapers/minimalistic/*
 do
   echo "  <a target=\"_blank\" href=\"$i\">
-    <img src=\"$i\" alt="$i" width=\"200\">
+    <img loading=\"lazy\" src=\"$i\" alt="$i" width=\"200\">
   </a>" >> ./index.html
 done
 echo "<h2>Mix</h2>" >>./index.html
 for i in ./wallpapers/mix/*
 do
   echo "  <a target=\"_blank\" href=\"$i\">
-    <img src=\"$i\" alt="$i" width=\"200\">
+    <img loading=\"lazy\" src=\"$i\" alt="$i" width=\"200\">
   </a>" >> ./index.html
 done
 echo "<h2>Pixelart</h2>" >>./index.html
 for i in ./wallpapers/pixelart/*
 do
   echo "  <a target=\"_blank\" href=\"$i\">
-    <img src=\"$i\" alt="$i" width=\"200\">
+    <img loading=\"lazy\" src=\"$i\" alt="$i" width=\"200\">
   </a>" >> ./index.html
 done
 echo "<h2>Anime</h2>" >>./index.html
 for i in ./wallpapers/anime/*
 do
   echo "  <a target=\"_blank\" href=\"$i\">
-    <img src=\"$i\" alt="$i" width=\"200\">
+    <img loading=\"lazy\" src=\"$i\" alt="$i" width=\"200\">
   </a>" >> ./index.html
 done
 echo "<h2>Light</h2>" >>./index.html
 for i in ./wallpapers/light/*
 do
   echo "  <a target=\"_blank\" href=\"$i\">
-    <img src=\"$i\" alt="$i" width=\"200\">
+    <img loading=\"lazy\" src=\"$i\" alt="$i" width=\"200\">
   </a>" >> ./index.html
 done
 
