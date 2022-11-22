a4d6686cfc562012cff7963bbd5253c7246b13cc
AngelJumbo <anarjumb@gmail.com>
2022-11-21 19:34:33 -0500

    Some improvements

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Gruvbox Wallpapers
 A place where to find gruvbox theme wallpapers.
 [https://gruvbox-wallpapers.onrender.com/](https://gruvbox-wallpapers.onrender.com/ 'Gruvbox Wallpapers')
-## Contrib
-Just add the wallpapers that you have in their respective folder and run the genPage.sh script
+## Contributions
+- Just add the wallpapers that you have in their respective folder.
+- Optionally you can run the genPage.sh script too.
+
+If your wallpaper does not fit in any of the folders we can debate the creation of a new one.
--- genPage.sh
+++ genPage.sh
@@ -12,23 +12,38 @@ echo "<!DOCTYPE html>
 </head>
 
 <body>
-  <h1>Wallpapers for gruvbox</h1>
-  <hr>
-  <p> Contrib <a href=\"https://github.com/AngelJumbo/gruvbox-wallpapers\">here</a> </p>" > ./index.html
-
+  <h1>Wallpapers for gruvbox</h1>" > ./index.html
+n=1
 for i in ./wallpapers/*
 do
   subdir=$i
-  echo "<h2>"${subdir##*/}"</h2>" >> ./index.html
  
+
+
+  echo "<h2" >> ./index.html
+  echo "id=s"$n">" >> ./index.html
+  echo "${subdir##*/}" | tr a-z A-Z  >> ./index.html
+  echo "</h2>" >> ./index.html
+  
+  echo "<div id=c>" >> ./index.html
+
   for j in ${subdir}/*
   do
     echo "  <a target=\"_blank\" href=\"$j\">
-    <img loading=\"lazy\" src=\"$j\" alt="$j" width=\"200\">
-  </a>" >> ./index.html
+    <img loading=\"lazy\" src=\"$j\" alt="$j" width=\"200\"></a>" >> ./index.html
   done
+  
+  echo "</div>" >> ./index.html
+
+  n=$((n+1))
+  if [ "$n" -eq 8 ]; then
+    n=1
+  fi
 done
 
-echo "</body>
-
+echo "<p> Contributions <a href=\"https://github.com/AngelJumbo/gruvbox-wallpapers\">here</a>.</p>
+<p> This images are from random sites or contributions so I don't have a way to know who are their original artist.</p>
+<p> I want to keep this site as simple as possible, but if you are the creator of any of these images and you want acknowledgment I will happily add a section with your name. Just open an issue <a href=\"https://github.com/AngelJumbo/gruvbox-wallpapers/issues\">here</a>.</p>
+<p> The same goes, if you want me to remove your art.</p>
+</body>
 </html>" >> ./index.html
