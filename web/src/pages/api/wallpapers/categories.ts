9aa5dedc9516e60cafbaffedcfe8a4274fa7fc2f
ChapST1 <cutitox59@gmail.com>
2024-01-16 19:46:46 -0500

    remove unnecessary code

--- web/src/pages/api/wallpapers/categories.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from "astro";
-import { getCategories } from "../../../services/external/get-categories";
-
-export const GET: APIRoute = async () => {
-	const categories = await getCategories({});
-
-	return new Response(JSON.stringify(categories));
-};
