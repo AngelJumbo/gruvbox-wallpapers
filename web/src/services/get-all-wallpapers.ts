c9cff785736521695e86852a5cf995bd23b20b3d
ChapST1 <cutitox59@gmail.com>
2024-01-16 21:44:06 -0500

    limit results

--- web/src/services/get-all-wallpapers.ts
+++ web/src/services/get-all-wallpapers.ts
@@ -1,11 +1,15 @@
 import { getCategories } from "@services/get-categories";
 import { getWallpapers } from "@services/get-wallpapers";
+import { IMAGES_FOR_PREVIEW } from "@lib/constants/index";
 
 export async function getAllWallpapers() {
 	const categories = await getCategories();
 
 	const allWallpapers = categories?.map(async (name) => {
-		const wallaperByCategory = await getWallpapers({ category: name });
+		const wallaperByCategory = await getWallpapers({
+			category: name,
+			limit: IMAGES_FOR_PREVIEW,
+		});
 		return wallaperByCategory;
 	});
 
