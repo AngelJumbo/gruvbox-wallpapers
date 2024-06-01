1db5c74b2c86c3661274952f0722534616589c32
Katy248 <petrovanton247@gmail.com>
2024-06-01 11:10:12 +0300

    Add viewport meta tag

--- build.sh
+++ build.sh
@@ -26,6 +26,7 @@ write_header(){
 
 <head>
   <meta charset=\"utf-8\">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
   <title>Gruvbox wallpapers</title>
   <script>
