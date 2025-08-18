d09bbc5645f47841fc2b3947413d1297dc5d6736
AngelJumbo <31261193+AngelJumbo@users.noreply.github.com>
2025-08-18 09:32:35 -0500

    Replace icons

--- build.sh
+++ build.sh
@@ -153,7 +153,6 @@ cat > index.html << 'EOF'
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <link rel='stylesheet' type='text/css' href='style.css'>
   <title>Gruvbox wallpapers</title>
-  <script src='https://kit.fontawesome.com/13865d7982.js' crossorigin='anonymous' defer></script>
   <style>
     .section-content {
       margin: 20px 0;
@@ -229,18 +228,29 @@ cat > index.html << 'EOF'
 </head>
 <body>
   <div class='float-btns'>
-    <a href='https://github.com/AngelJumbo/gruvbox-wallpapers' target='_blank' class='btn float-btn' title='Source code' >
+    <a href='https://github.com/AngelJumbo/gruvbox-wallpapers' target='_blank' class='btn float-btn' title='Source code'>
       <span>
-        <i class='fa-brands fa-github'></i>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2"
+          stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github">
+          <path d="M12 1C5.37 1 0 6.37 0 13c0 5.3 3.44 9.8 8.21 11.39.6.11.82-.26.82-.58 0-.28-.01-1.02-.02-2-3.34.73-4.04-1.61-4.04-1.61-.55-1.4-1.34-1.77-1.34-1.77-1.1-.75.08-.73.08-.73 1.22.09 1.87 1.25 1.87 1.25 1.08 1.86 2.82 1.32 3.51 1.01.11-.8.42-1.32.76-1.63-2.67-.31-5.48-1.34-5.48-5.95 0-1.31.47-2.38 1.25-3.22-.13-.31-.54-1.55.12-3.23 0 0 1.01-.32 3.3 1.23a11.45 11.45 0 013-.41c1.02 0 2.05.14 3 .41 2.29-1.55 3.3-1.23 3.3-1.23.66 1.68.25 2.92.12 3.23.78.84 1.25 1.91 1.25 3.22 0 4.62-2.81 5.63-5.49 5.94.43.37.81 1.11.81 2.24 0 1.62-.02 2.93-.02 3.33 0 .32.22.7.83.58C20.56 22.8 24 18.3 24 13c0-6.63-5.37-12-12-12z"/>
+        </svg>
       </span>
     </a>
     <button onclick='switchMainTheme()' class='btn float-btn' title='Switch theme'>
       <span>
-        <i id='light-icon' class='fa-solid fa-sun'></i>
-        <i id='dark-icon' class='fa-solid fa-moon'></i>
+        <svg id="light-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2"
+          stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun">
+          <circle cx="12" cy="12" r="5"/>
+          <path d="M12 1v2m0 18v2m11-11h-2M3 12H1m16.95 6.95-1.41-1.41M6.46 6.46 5.05 5.05m12.9 0-1.41 1.41M6.46 17.54 5.05 18.95"/>
+        </svg>
+        <svg id="dark-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2"
+          stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon">
+          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>
+        </svg>
       </span>
     </button>
   </div>
+
   <main>
     <h1>Gruvbox Wallpapers</h1>
 EOF
